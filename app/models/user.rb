0dda636cf8de61fe186647bd07858d197c6c35d2
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-26 16:25:38 +0800

    Changes maximum lengths allowed for username, lastname, firstname

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :firstname, presence: true, length: {minimum:2, maximum:20}
-  validates :lastname, presence: true, length: {minimum:2, maximum:20}
-  validates :username, presence: true, length: {minimum:2, maximum:20}, uniqueness: true;
+  validates :firstname, presence: true, length: {minimum:2, maximum:15}
+  validates :lastname, presence: true, length: {minimum:2, maximum:15}
+  validates :username, presence: true, length: {minimum:2, maximum:25}, uniqueness: true;
 end
