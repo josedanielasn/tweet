6b483465a4ed19da5f61bc775bf5b9c3841bac7b
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-26 15:53:40 +0800

    Adds factory for user / Integrates faker gem to generate inputs for user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    
+    firstname {Faker::Name.first_name}
+    lastname {Faker::Name.last_name}
+    username {Faker::Internet.username(specifier: 2..15)}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(min_length: 6)}
   end
 end
