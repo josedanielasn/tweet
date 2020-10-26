0c224b9fc8cbb3da4b7ecfd75e6915bebaa85353
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-26 16:25:05 +0800

    Adds specifier to method of faker to specify length of generated strings for username, lastname, firstname

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    firstname {Faker::Name.first_name}
-    lastname {Faker::Name.last_name}
-    username {Faker::Internet.username(specifier: 2..15)}
+    firstname {Faker::Internet.username(specifier: 2..15)}
+    lastname {Faker::Internet.username(specifier: 2..15)}
+    username {Faker::Internet.username(specifier: 2..25)}
     email {Faker::Internet.email}
     password {Faker::Internet.password(min_length: 6)}
   end
