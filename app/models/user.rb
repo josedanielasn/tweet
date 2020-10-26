f460346fd710e6ad73bb99a555e8f6f9eafa0c8c
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-26 19:14:43 +0800

    Add data association between post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :firstname, presence: true, length: {minimum:2, maximum:15}
