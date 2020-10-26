3633b511a7533ae4bce7f7c3f884a8759b9fe681
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-26 19:15:43 +0800

    Adds testing for validation of posts

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context "when validated" do
+    context "when post is valid" do
+      let(:post) { build(:post) }
+
+      it { expect(post).to be_valid }
+    end
+
+    context "when message of post is invalid" do
+      let(:post1) { build(:post, message: nil)}
+      let(:post2) { build(:post, message: "1" * 301)}
+
+      it { expect(post1).not_to be_valid }
+      it { expect(post2).not_to be_valid }
+    end
+    
+    
+  end
+  
+end
