a8c14f2e87287a53427fbffa472399a10d4d1435
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-28 14:07:12 +0800

    Adds relationship table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_104943) do
+ActiveRecord::Schema.define(version: 2020_10_27_071747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_10_26_104943) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "followed_id"
+    t.integer "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
