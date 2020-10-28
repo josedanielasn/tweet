511986cd134d0110947ac2ee530fe7cf52e41440
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-28 13:14:10 +0800

    Adds restriction for deleting post to be only accessible to owner of post / Fixes redirection after deleting post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
-  before_action :find_post, except: [:index, :new, :create]
+
   def index
     @posts = Post.all
     @post = current_user.posts.build
   end
 
-  def show; end
+  def show
+    @post = Post.find_by(id: params[:id]) 
+    return redirect_to user_path(current_user) if @post == nil
+    @user = @post.user
+  end
 
   def create
     @post = current_user.posts.build(posts_params)
@@ -13,26 +17,14 @@ class PostsController < ApplicationController
     render 'new'
   end
 
-  def edit; end
-
-  def update
-    return redirect_to user_post_path(@user, @post) if @post.update(posts_params)
-    render 'edit'
-  end
-
   def destroy
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     go_back
   end
 
   private
-
     def posts_params
       params.require(:post).permit(:message)
-    end
-
-    def find_post
-      @post = Post.find(params[:id])
-      @user = @post.user
     end
 end
