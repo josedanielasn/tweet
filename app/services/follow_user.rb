8f664b25da6b4d615d8308cea4556e4391d889f4
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-28 15:47:39 +0800

    Adds class Follow, to parse if current_user if following the user

--- app/services/follow_user.rb
+++ app/services/follow_user.rb
@@ -1,19 +1,21 @@
-module Follow
+
   class FollowUser 
     attr_accessor :follow_account
   
-    def initialize(follow_account)
-      @current_account = current_account
-      @follow_account = follow_account
+    def initialize(follow_account, current_user)
+      @current_account = current_user
+      # byebug
+      @follow_account = User.find(follow_account)
     end
     
     def call 
-      followers = current_user.followed_users
-      if followers.find(@follow_account)
-        followers << @follow_account
+      followed_users = @current_account.followed_users
+      # byebug
+      if followed_users.find_by(id: @follow_account.id) == nil
+        followed_users << @follow_account
       else
-        followers.followed_users.delete(@followed_account)
+        followed_users.delete(@follow_account)
       end
     end
   end
-end
+
