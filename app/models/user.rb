bba633153fa8e08e308205c65f9c16fc6087d679
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-29 13:04:37 +0800

    Associates user model to retweet model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
+  has_many :retweets, foreign_key: "reposter_id", dependent: :destroy
+  has_many :reposts, through: :retweets, source: :repost
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
