d7f70592adf2f208e835945411ca3e1ecabdac39
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-30 11:55:20 +0800

    Adds mentions table in schema

--- db/migrate/20201030021607_create_mentions.rb
+++ db/migrate/20201030021607_create_mentions.rb
@@ -1,7 +1,8 @@
 class CreateMentions < ActiveRecord::Migration[6.0]
   def change
     create_table :mentions do |t|
-
+      t.integer :mentioner_id
+      t.integer :mentionee_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_081215) do
+ActiveRecord::Schema.define(version: 2020_10_30_021607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mentions", force: :cascade do |t|
+    t.integer "mentioner_id"
+    t.integer "mentionee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
