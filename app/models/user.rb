bcceee3edcdb27448b40a3dcffa7fa8119963088
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-30 12:04:14 +0800

    Associates mention to user modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :mentions, foreign_key: 'mentionee_id', dependent: :destroy
+  has_many :mentioners, through: :mentions, source: :mentioner
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :firstname, presence: true, length: {minimum:2, maximum:15}
