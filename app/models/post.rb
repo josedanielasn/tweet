2e21f311c001aa2d9ab9b87236ee4096e3788108
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-01 17:01:24 +0800

    Removes association of these models to retweet model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :retweets, foreign_key: "repost_id", dependent: :destroy
-  has_many :reposters, through: :retweets, source: :reposter
   validates :message, presence: true, length: {maximum:280}
 end
