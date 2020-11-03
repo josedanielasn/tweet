4e584504f2bf87472faf8813a9564523c0b62526
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-03 11:10:54 +0800

    Changes pagination from 5 items per page to 10 items per page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+    @posts = @user.posts.paginate(page: params[:page], per_page: 10).order('created_at DESC')
     @is_following = current_user.followed_users.find_by(id: params[:id]) 
   end
 
